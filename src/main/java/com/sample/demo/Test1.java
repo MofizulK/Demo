0f7b6779680fa311671da543fb0d6591ecc7f88a
MofizulK <mofizkarimtiu@gmail.com>
2022-01-16 10:43:32 +0530

    Second commit

--- src/main/java/com/sample/demo/Test1.java
+++ src/main/java/com/sample/demo/Test1.java
@@ -2,6 +2,6 @@ package com.sample.demo;
 
 public class Test1 {
        public void myDemo() {
-    	   System.out.println("we r here");
+    	   System.out.println("YAAAAAAAAAAAAYYY");
        }
 }
